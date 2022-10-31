08ec3571069f8f9621612ef3223937db422f55f2
root <2369561300@qq.com>
2022-10-31 19:37:29 +0800

    new version of e3nngen, further improve the data transmission and md explore, fix the bug

--- e3nn_init/reason_search.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-from glob import glob
-import numpy as np
-
-dirs = glob('./task.*')
-for dir0 in dirs:
-    reason = np.loadtxt(dir0+'/reasonable.txt')
-    if np.sum(reason) < len(reason):
-        print(dir0)
