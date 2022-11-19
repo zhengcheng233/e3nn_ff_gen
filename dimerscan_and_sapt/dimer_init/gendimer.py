cc3e1a9fe74264846e2bb6dbc64c991cd0285c80
root <2369561300@qq.com>
2022-11-19 22:38:47 +0800

    change the dimer config gen

---
+++ dimerscan_and_sapt/dimer_init/gendimer.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+"""
+generate dimer config, first we find the farthest two atoms and the mass center; then according to the direction 
+move the monomer, find the smallest distance between the monomer. If the distance arrive the threshold, then stop
+"""
---
+++ dimerscan_and_sapt/dimer_init/readme.txt
@@ -0,0 +1,1 @@
+used to generated initial dimer configs
